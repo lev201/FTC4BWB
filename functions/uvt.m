3033bfd81474aea55d42ef8f53f2d2d6f057056e
Liam Vile <lev201@outlook.ac.uk>
2018-05-27 20:00:18 +0100

    Issue in matrix dimensions for Ts matrix

--- functions/uvt.m
+++ functions/uvt.m
@@ -13,8 +13,8 @@ for i = 1:nn
     end
 end
 
-S1=S(1:size(S,1), 1:p);
-S2=S(1:size(S,1), p+1:size(S,2));
+S1=S(1:size(S,1), 1:p-1);
+S2=S(1:size(S,1), p:size(S,2));
 
 Ts=[eye(size(S1,2)) zeros(size(S1,2), size(S2,1))
     S1               S2];
