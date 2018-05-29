bc0ed9ea5c66767ffca0887da258fc1cf854c123
lev201 <39651547+lev201@users.noreply.github.com>
2018-05-29 18:10:24 +0100

    Issue with indexing in partitions fixed

--- functions/parti.m
+++ functions/parti.m
@@ -1,4 +1,6 @@
 function [A B p] =parti(A, B)
+A=BWBlat.A
+B=Bv
 
 [nn, mm] = size (B);
 
@@ -14,10 +16,10 @@ for i = 1:nn
     end
 end
 
-A11=A(1:p-1, 1:p-1);
-A12=A(1:p-1, p:nn);
-A21=A(p:nn, 1:p-1);
-A22=A(p:nn, p:nn);
+A11=A(1:p, 1:p);
+A12=A(1:p, p+1:nn);
+A21=A(p+1:nn, 1:p);
+A22=A(p+1:nn, p+1:nn);
 
 A=cell(2,2);
 A{1,1}=A11;
@@ -25,8 +27,8 @@ A{1,2}=A12;
 A{2,1}=A21;
 A{2,2}=A22;
 
-B1=B(1:p-1, 1:mm);
-B2=B(p:nn, 1:mm);
+B1=B(1:p, 1:mm);
+B2=B(p+1:nn, 1:mm);
 
 B=cell(2,1);
 B{1,1}=B1;
