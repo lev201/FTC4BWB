aaf8161f5de85eae209128189b35fd1e185a0f1f
lev201 <39651547+lev201@users.noreply.github.com>
2018-05-29 18:13:58 +0100

    Bug fixed for sizing issues in Q partition

--- functions/SQM.m
+++ functions/SQM.m
@@ -1,13 +1,12 @@
 function [S, E]=SQM(A, B, Q);
-
 nn=size(A,1);
-[A,B]=stdfrm(A, B);
+
 [A B p]=parti(A, B);
 
-Q11=Q(1:p-1, 1:p-1);
-Q12=Q(1:p-1, p:nn);
-Q21=Q(p:nn, 1:p-1);
-Q22=Q(p:nn, p:nn);
+Q11=Q(1:p, 1:p);
+Q12=Q(1:p, p+1:nn);
+Q21=Q(p+1:nn, 1:p);
+Q22=Q(p+1:nn, p+1:nn);
 
 A_hat=A{1,1}-A{1,2}*inv(Q22)*Q21;
 
@@ -17,7 +16,7 @@ P1=care(A_hat, A{1,2}, Q_hat);
 M=inv(Q22)*(A{1,2}'*P1+Q21);
 
 E=eig(A{1,1}-A{1,2}*M);
-S2=eye(p);
+S2=eye(p+1);
 S1=[S2*M];
 S=[S1 S2];
 
